85340d027aa6f029addc019b997e83c30293a23e
odi parl <pearlodi7@gmail.com>
2024-07-30 15:56:42 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
