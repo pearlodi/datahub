5add66d84ec74bfa4bf2c0c461605dbe845f7a4a
odi parl <pearlodi7@gmail.com>
2024-08-07 02:19:27 +0100

    website migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Provider } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import store from './redux/store';
 
-function App() {
+const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Hello, Tailwind CSS!
-      </h1>
-    </div>
+    <Provider store={store}>
+      <Outlet />
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/layouts/main-layout/sidebar/list-items/ProfileListItem.tsx
@@ -0,0 +1,25 @@
+
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import ListItemButton from '@mui/material/ListItemButton';
+import {MenuItem} from '../../../../routes/sitemap';
+
+const ProfileListItem = ({ subheader, path }: MenuItem) => {
+  return (
+    <ListItemButton component={Link} href={path}>
+      <Stack spacing={1} alignItems="center">
+        <Stack direction="column">
+          <Typography variant="subtitle2" color="text.primary" letterSpacing={0.5}>
+            {subheader}
+          </Typography>
+          <Typography variant="caption" color="text.secondary" fontWeight={400}>
+            Account Settings
+          </Typography>
+        </Stack>
+      </Stack>
+    </ListItemButton>
+  );
+};
+
+export default ProfileListItem;
